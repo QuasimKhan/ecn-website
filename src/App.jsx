f2e33f6bb8825d288e72579800df8dceb26814d0
QuasimKhan <123313112+QuasimKhan@users.noreply.github.com>
2024-07-31 03:18:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 
 import './App.css'
-import NavBar from './components/Navbar/NavBar'
-import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/Navbar/NavBar 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home'
 import About from './components/About/About'
 import Services from './components/Services/Services'
 import Books from './components/Books/Books'
 import Footer from './components/Footer/Footer'
+import PageNotFound from './components/PageNotFound'
 function App() {
 
   return (
     <>
     <NavBar/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/services' element={<Services/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/Books' element={<Books/>}/>
-    </Routes>
+    <Router>
+      <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/services' component={Services}/>
+      <Route exact path='/about' component={About}/>
+      <Route exact path='/Books' component={Books}/>
+        <Route component={PageNotFound}/>
+    </Switch>
+      </Router>
     <Footer/>
     </>
   )
