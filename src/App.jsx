05bf6b0e80b132a7ea752e4d72cff5ca84cb84b3
QuasimKhan <quasimkhanpersonal@gmail.com>
2024-08-03 11:42:02 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import './App.css'
+import './App.css';
 import NavBar from './components/Navbar/NavBar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Services from './components/Services/Services';
 import Books from './components/Books/Books';
 import Footer from './components/Footer/Footer';
 
-
 function App() {
   return (
-    <Router>
-      <>
-        <NavBar />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/services' component={Services} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/books' component={Books} />
-        </Switch>
-        <Footer />
-      </>
-    </Router>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/books' element={<Books />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
