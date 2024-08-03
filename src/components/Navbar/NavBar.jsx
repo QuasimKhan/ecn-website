03261aa61a57255e33c412f646121e6ff39bc68f
QuasimKhan <quasimkhanpersonal@gmail.com>
2024-08-03 11:47:29 +0530

    Updated

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import ToggleSwitch from './ToggleSwitch'; // Import the toggle switch component
-import logo from './logo.png'; // Import the logo image
-import { useTheme } from './ThemeContext'; // Import the theme context
+import ToggleSwitch from "./ToggleSwitch"; // Import the toggle switch component
+import logo from "./logo.png"; // Import the logo image
+import { useTheme } from "./ThemeContext"; // Import the theme context
 
 export default function Navigation() {
   const [open, setOpen] = useState(false);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const location = useLocation();
   const { darkMode, toggleDarkMode } = useTheme();
 
@@ -20,59 +20,104 @@ export default function Navigation() {
 
   const handleToggleDarkMode = () => {
     toggleDarkMode();
-    const newMessage = darkMode ? 'Light mode enabled' : 'Dark mode enabled';
+    const newMessage = darkMode ? "Light mode enabled" : "Dark mode enabled";
     setMessage(newMessage);
-    setTimeout(() => setMessage(''), 3000); // Hide message after 3 seconds
+    setTimeout(() => setMessage(""), 3000); // Hide message after 3 seconds
   };
 
   return (
     <>
       <div className="flex justify-center w-full fixed top-5">
-        <nav className={`h-20 w-5/6 rounded-lg flex items-center justify-between p-4 transition-colors duration-300 dark:bg-gray-900 dark:shadow-[0_0_10px_5px_rgba(59,130,246,1)] bg-gray-800 shadow-[0_0_10px_5px_rgba(211,211,211,1)] text-white z-50`}>
+        <nav
+          className={`h-20 w-5/6 rounded-lg flex items-center justify-between p-4 transition-colors duration-300 dark:bg-gray-900 dark:shadow-[0_0_10px_5px_rgba(59,130,246,1)] bg-gray-800 shadow-[0_0_10px_5px_rgba(211,211,211,1)] text-white z-50`}
+        >
           <a href="/" className="flex items-center justify-center z-30">
-            <img src={logo} alt="logo" className="logo w-32 h-32 md:w-44 md:h-44 object-contain" />
+            <img
+              src={logo}
+              alt="logo"
+              className="logo w-32 h-32 md:w-44 md:h-44 object-contain"
+            />
           </a>
           <div className="flex items-center">
-            <ul className={`flex flex-col justify-end md:flex-row md:space-x-4 fixed md:static top-0 left-0 w-full md:w-auto h-1/2 md:h-auto bg-gray-800 md:bg-transparent transition-transform transform ${open ? "translate-x-0" : "-translate-x-full"} md:translate-x-0 z-20 md:z-auto`}>
+            <ul
+              className={`flex flex-col justify-end md:flex-row md:space-x-4 fixed md:static top-0 left-0 w-full md:w-auto h-4/6 md:h-auto bg-gray-800 md:bg-transparent transition-transform transform ${
+                open ? "translate-x-0" : "-translate-x-full"
+              } md:translate-x-0 z-20 md:z-auto`}
+            >
               <li className="md:hidden flex justify-end p-4">
                 <button onClick={toggleMenu} className="text-2xl">
                   <i className="fas fa-times"></i>
                 </button>
               </li>
               <li className="flex justify-center md:justify-start p-4">
-                <ToggleSwitch darkMode={darkMode} onToggle={handleToggleDarkMode} />
+                <ToggleSwitch
+                  darkMode={darkMode}
+                  onToggle={handleToggleDarkMode}
+                />
               </li>
               <li className="flex justify-center md:justify-start p-4">
-                <Link to="/" className={`hover:text-gray-400 ${isActive("/")}`} onClick={toggleMenu}>
+                <Link
+                  to="/"
+                  className={`hover:text-gray-400 ${isActive("/")}`}
+                  onClick={toggleMenu}
+                >
                   Home
                 </Link>
               </li>
               <li className="flex justify-center md:justify-start p-4">
-                <Link to="/about" className={`hover:text-gray-400 ${isActive("/about")}`} onClick={toggleMenu}>
+                <Link
+                  to="/about"
+                  className={`hover:text-gray-400 ${isActive("/about")}`}
+                  onClick={toggleMenu}
+                >
                   About
                 </Link>
               </li>
               <li className="flex justify-center md:justify-start p-4">
-                <Link to="/services" className={`hover:text-gray-400 ${isActive("/services")}`} onClick={toggleMenu}>
+                <Link
+                  to="/services"
+                  className={`hover:text-gray-400 ${isActive("/services")}`}
+                  onClick={toggleMenu}
+                >
                   Services
                 </Link>
               </li>
               <li className="flex justify-center md:justify-start p-4">
-                <Link to="/books" className={`hover:text-gray-400 ${isActive("/books")}`} onClick={toggleMenu}>
+                <Link
+                  to="/books"
+                  className={`hover:text-gray-400 ${isActive("/books")}`}
+                  onClick={toggleMenu}
+                >
                   Books
                 </Link>
               </li>
               <li className="flex justify-center md:justify-start p-4">
-                <Link to="/contact" className={`hover:text-gray-400 ${isActive("/contact")}`} onClick={toggleMenu}>
+                <Link
+                  to="/contact"
+                  className={`hover:text-gray-400 ${isActive("/contact")}`}
+                  onClick={toggleMenu}
+                >
                   Contact
                 </Link>
               </li>
             </ul>
             <button className="md:hidden ml-4 z-30" onClick={toggleMenu}>
               <div className="space-y-1">
-                <span className={`block h-0.5 w-6 bg-current transform transition duration-500 ${open ? "rotate-45 translate-y-1.5" : ""}`}></span>
-                <span className={`block h-0.5 w-6 bg-current transition duration-500 ${open ? "opacity-0" : ""}`}></span>
-                <span className={`block h-0.5 w-6 bg-current transform transition duration-500 ${open ? "-rotate-45 -translate-y-1.5" : ""}`}></span>
+                <span
+                  className={`block h-0.5 w-6 bg-current transform transition duration-500 ${
+                    open ? "rotate-45 translate-y-1.5" : ""
+                  }`}
+                ></span>
+                <span
+                  className={`block h-0.5 w-6 bg-current transition duration-500 ${
+                    open ? "opacity-0" : ""
+                  }`}
+                ></span>
+                <span
+                  className={`block h-0.5 w-6 bg-current transform transition duration-500 ${
+                    open ? "-rotate-45 -translate-y-1.5" : ""
+                  }`}
+                ></span>
               </div>
             </button>
           </div>
