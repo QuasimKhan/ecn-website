58580cd54b49fabd539662cb1a3f7a8b3edd1142
QuasimKhan <quasimkhanpersonal@gmail.com>
2024-08-03 11:28:26 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import About from './components/About/About';
 import Services from './components/Services/Services';
 import Books from './components/Books/Books';
 import Footer from './components/Footer/Footer';
-import PageNotFound from './components/PageNotFound';
+
 
 function App() {
   return (
@@ -18,7 +18,6 @@ function App() {
           <Route exact path='/services' component={Services} />
           <Route exact path='/about' component={About} />
           <Route exact path='/books' component={Books} />
-          <Route component={PageNotFound} />
         </Switch>
         <Footer />
       </>
